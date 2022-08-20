de695e75e52c9e540aebd9b8a81b69b85e832144
dima423427 <dkorolyonok@mail.ru>
2022-08-20 19:37:20 +0700

    Привести структуру проекта в порядок !!!

--- Startup.cs
+++ Startup.cs
@@ -12,10 +12,12 @@ namespace AnimeLib
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Поддержка контроллеров и представлений MVC
+            services.AddControllersWithViews()
+                //Совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +30,12 @@ namespace AnimeLib
 
             app.UseRouting();
 
+            //Поддержка статичных файлов в приложении (js,css...)
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
