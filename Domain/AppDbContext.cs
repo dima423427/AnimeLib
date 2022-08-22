58fef74650d67c606036e1321b4541353bed61eb
dima423427 <dkorolyonok@mail.ru>
2022-08-22 17:12:10 +0700

    4 19.45

---
+++ Domain/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AnimeLib.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimeLib.Domain
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TextField> TextFields { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TextField>()
+        }
+    }
+}
--- Domain/Entities/EntityBase.cs
+++ Domain/Entities/EntityBase.cs
@@ -1,8 +1,38 @@
-﻿namespace AnimeLib.Domain.Entities
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnimeLib.Domain.Entities
 {
     public abstract class EntityBase
     {
+        protected EntityBase() => DateAdded = DateTime.UtcNow;
+
         [Required]
-        public Guid Id { get; set; } 
+        public Guid Id { get; set; } //Первичный ключ
+
+        [Display(Name = "Название(заголовок)")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Краткое описание")]
+        public virtual string Subtitle { get; set; }
+
+        [Display(Name = "Полное описание")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "Титульная картинка")]
+        public virtual string TitleImagePath { get; set; }
+
+        [Display(Name = "SEO метатег Title")]
+        public virtual string MetaTitle { get; set; }
+
+        [Display(Name = "SEO метатег Description")]
+        public virtual string MetaDescription { get; set; }
+
+        [Display(Name = "SEO метатег Keywords")]
+        public virtual string MetaKeywords { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+
     }
 }
